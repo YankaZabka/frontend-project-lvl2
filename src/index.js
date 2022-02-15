a7a6cdc9afadf3ea6a3b0d84b71707b13e51e4df
YankaZabka <yanka.zanka@gmail.com>
2022-02-15 18:59:51 +0300

    Run eslint --fix .

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import _ from 'lodash';
 
-const readFile = (filepath) => {
-  return fs.readFileSync(path.resolve(process.cwd(), filepath.trim()), 'utf-8');
-};
+const readFile = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath.trim()), 'utf-8');
 
 const genDiff = (filePath1, filePath2) => {
   const data1 = JSON.parse(readFile(filePath1));
@@ -12,34 +10,33 @@ const genDiff = (filePath1, filePath2) => {
   const data1Keys = Object.keys({ ...data1 });
   const data2Keys = Object.keys({ ...data2 });
 
-  const diffObj = data1Keys.map(key1 => {
+  const diffObj = data1Keys.map((key1) => {
     if (data1[key1] === data2[key1]) {
       return {
         propertyName: key1,
         propertyValue: data1[key1],
-        diffValue: ' '
+        diffValue: ' ',
       };
-    } else if (data1[key1] !== data2[key1]) {
+    } if (data1[key1] !== data2[key1]) {
       return {
         propertyName: key1,
         propertyValue: data1[key1],
-        diffValue: '-'
+        diffValue: '-',
       };
     }
+    return null;
   });
-  data2Keys.forEach(key2 => {
+  data2Keys.forEach((key2) => {
     if (data1[key2] !== data2[key2]) {
       diffObj.push({
         propertyName: key2,
         propertyValue: data2[key2],
-        diffValue: '+'
+        diffValue: '+',
       });
     }
   });
   const result = _.sortBy(diffObj, 'propertyName')
-    .map(obj => {
-      return `  ${obj.diffValue} ${obj.propertyName}: ${obj.propertyValue}`;
-    });
+    .map((obj) => `  ${obj.diffValue} ${obj.propertyName}: ${obj.propertyValue}`);
   const outputString = `{\n${result.join('\n')}\n}`;
   console.log(outputString);
 };
