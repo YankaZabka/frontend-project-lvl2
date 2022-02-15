1256014e90a2e0f209c40eec6065368232d20406
YankaZabka <yanka.zanka@gmail.com>
2022-02-15 19:58:08 +0300

    Add index.test.js file

---
+++ __tests__/src/index.test.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@jest/globals';
+import genDiff from '../../src/index.js';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+test("genDiff test", () => {
+  const filePath1 = path.join(__dirname, "../..", "__fixtures__/file1.json")
+  const filePath2 = path.join(__dirname, "../..", "__fixtures__/file2.json")
+  const rightOutput = "{\n" +
+    "  - follow: false\n" +
+    "    host: hexlet.io\n" +
+    "  - proxy: 123.234.53.22\n" +
+    "  - timeout: 50\n" +
+    "  + timeout: 20\n" +
+    "  + verbose: true\n" +
+    "}"
+  expect(genDiff(filePath1, filePath2)).toBe(rightOutput)
+})
