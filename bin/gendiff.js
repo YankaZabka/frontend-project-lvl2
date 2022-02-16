bc45ea39b967a105b36ff250d0fe77bdd6f5ab20
YankaZabka <yanka.zanka@gmail.com>
2022-02-16 13:04:00 +0300

    Add default format option to commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,9 @@ program
   .version('0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, { format }) => {
+    console.log(genDiff(filepath1, filepath2, format));
   });
 
 program.parse();
